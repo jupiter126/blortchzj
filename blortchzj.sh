78df30343e4fb00f5aa9f7cbb3f7c353351be332
JuPiTeR126 <jupiter126@gmail.com>
2017-12-13 12:07:49 +0100

    modified:   blortchzj.sh

--- blortchzj.sh
+++ blortchzj.sh
@@ -2,8 +2,9 @@
 # default password length is 19 - call app with a number as parameter to override
 plength=19
 pname=$(echo "$0"|sed -e 's/.sh//' -e 's\./\\')
-echo "oh no! my name is $pname, please rename me for enthropy's sake!"
-echo "please tell me my new name:"
+def=$(tput sgr0);bol=$(tput bold);red=$(tput setaf 1;tput bold);gre=$(tput setaf 2;tput bold);yel=$(tput setaf 3;tput bold);blu=$(tput setaf 4;tput bold);mag=$(tput setaf 5;tput bold);cya=$(tput setaf 6;tput bold)
+echo "$yel oh no! my name is $pname, please rename me for enthropy's sake!$def"
+echo "$gre Please tell me my new name:$def"
 read pname
 if [[ "$1" != "" ]]; then
 	re='^[0-9]+$'
@@ -15,27 +16,29 @@ if [[ "$1" != "" ]]; then
 fi
 ppass="$pname"
 # Feed stuff to app to initialise
-echo "Hello, my name is $pname, tell me some stuff"
+echo "$gre Hello, my name is $pname, tell me some stuff$def"
 read panswer
 if [[ "$panswer" = "" ]]; then
 	echo "No text, no pass..."
 	exit 2
 fi
 ppass="$ppass-$panswer"
+echo "$bol Once done, press enter on a blank line to go to next step$def"
 while [[ "$panswer" != "" ]]; do
-	echo "Anything more to tell me? (once done, press enter on a blank line to go to next step)"
+	echo "$gre Anything more to tell me?$def"
 	read panswer
 	ppass="$ppass-$panswer"
 done
 panswer="lol"
 #retrieve your passwords
 echo "I hope I know enough, now I'll give you passwords"
+echo "$bol Once done, press enter on a blank line to exit script$def"
 while [[ "$panswer" != "" ]]; do
-	echo "What pass would you like to know? (once done, press enter on a blank line to exit script)"
+	echo "$gre What pass would you like to know?$def"
 	read panswer
 	if [[ "$panswer" != ""  ]]; then
 		pppasss="$ppass-$panswer"
-		echo "$pppasss"|md5sum|sha512sum|base64|head -c$plength
+		echo "$red $(echo $pppasss|md5sum|sha512sum|base64|head -c$plength)$def"
 		echo " "
 	fi
 done
